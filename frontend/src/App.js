2f6124d17cd94fa968827c0a3596073b304a0cb4
Nuruddin Tipu <nuruddintipu.connect@gmail.com>
2025-02-09 05:15:43 +0600

    Fix indentation across project for consistent formatting using prettier

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,7 @@
 import './styles/App.css';
 
 function App() {
-  return (
-    <div className="App">
-      Hello World
-    </div>
-  );
+    return <div className="App">Hello World</div>;
 }
 
 export default App;
