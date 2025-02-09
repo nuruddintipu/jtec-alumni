0b38280a47b9a8de7bb2000def910e468ba3ba1f
Nuruddin Tipu <nuruddintipu.connect@gmail.com>
2025-02-09 07:01:48 +0600

    Add Homepage component with LandingSection setup
    Introduce the `Homepage` component to serve as the main entry point for the application. It includes the `LandingSection` component with placeholder text and applies associated CSS styling. This sets up the foundation for the homepage layout.

---
+++ frontend/src/pages/homepage/Homepage.js
@@ -0,0 +1,16 @@
+import LandingSection from '../../components/LandingSection';
+import './homepage.css';
+
+const Homepage = () => {
+    return (
+        <>
+            <LandingSection
+                title={'Welcome to JTec Alumni'}
+                subtitle={'A place to share your experiences with the JTec community'}
+                body={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, diam quis efficitur efficitur, nisi nisi semper est, vitae efficitur est nisi quis nisi.'}
+            />
+        </>
+    );
+};
+
+export default Homepage;
