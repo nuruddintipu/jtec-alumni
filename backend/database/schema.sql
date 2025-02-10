42fa401a97e655fda756ba29e7a4a79acbe54065
Nuruddin Tipu <nuruddintipu.connect@gmail.com>
2025-02-10 06:48:29 +0600

    Add initial database schema with users and notices tables
    Introduces `users` and `notices` tables to establish the core database structure. This is the foundational setup for the application's database.

---
+++ backend/database/schema.sql
@@ -0,0 +1,20 @@
+CREATE TABLE users (
+    id CHAR(36) PRIMARY KEY DEFAULT (UUID()),
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+);
+
+
+CREATE TABLE notices (
+    id CHAR(36) PRIMARY KEY DEFAULT (UUID()),
+    title VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    creator_id CHAR(36) NOT NULL,
+    views INT DEFAULT 0,
+    published_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (creator_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
