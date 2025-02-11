e1f7369c77ed4c1f47a3616d93f28e49b4d8aad6
Nuruddin Tipu <nuruddintipu.connect@gmail.com>
2025-02-11 08:55:44 +0600

    Add CORS headers in the headers.php utility file
    Configured headers to allow cross-origin requests from localhost for development purposes. This includes enabling POST requests and specific headers for improved API interaction.

---
+++ backend/utils/headers.php
@@ -0,0 +1,5 @@
+<?php
+header("Access-Control-Allow-Origin: http://localhost:3000");
+header("Content-Type: application/json");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
